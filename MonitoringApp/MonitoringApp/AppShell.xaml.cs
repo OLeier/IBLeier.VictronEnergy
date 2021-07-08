234decc750b7bb91fce056a2cd519ddeb7b12f7e
O.Leier <github@ib-leier.net>
2021-07-08 19:10:37 +0200

    MonitoringApp added

---
+++ MonitoringApp/MonitoringApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MonitoringApp.ViewModels;
+using MonitoringApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MonitoringApp
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+			Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+		}
+
+	}
+}
---
+++ MonitoringApp/MonitoringApp/ViewModels/SolarViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitoringApp.ViewModels
+{
+	public class SolarViewModel : BaseViewModel
+	{
+	}
+}
